0287c64ebd98300e6ae9c36c26d6fe7131ae73eb
Chaho Choi <dhfkdhaneod@gmail.com>
2023-06-14 12:28:32 +0900

    first commit

---
+++ src/main/java/com/soroksorokk/soroksorokk/SoroksorokkApplication.java
@@ -0,0 +1,13 @@
+package com.soroksorokk.soroksorokk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoroksorokkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoroksorokkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soroksorokk/soroksorokk/SoroksorokkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soroksorokk.soroksorokk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoroksorokkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
