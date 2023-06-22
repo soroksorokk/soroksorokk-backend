7a2314b67a7945bffa3809ac14fca02f409d3933
Chaho Choi <dhfkdhaneod@gmail.com>
2023-06-22 15:29:54 +0900

    [Fix] cors 수정

--- src/main/java/com/soroksorokk/soroksorokk/config/CorsConfiguration.java
+++ src/main/java/com/soroksorokk/soroksorokk/config/CorsConfiguration.java
@@ -6,18 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
-    @Bean
-    public WebMvcConfigurer corsConfiguration() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
-
 }
