c144a14eeb5b79780a78fdd64b60a026d2410c1e
Chaho Choi <dhfkdhaneod@gmail.com>
2023-06-22 12:55:57 +0900

    #1 Cors 기본 설정

---
+++ src/main/java/com/soroksorokk/soroksorokk/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.soroksorokk.soroksorokk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
