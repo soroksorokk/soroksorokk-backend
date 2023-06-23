77fcf4b8cfa99c53c7d3b0d2d21ff4feb8447c64
Chaho Choi <dhfkdhaneod@gmail.com>
2023-06-23 14:22:55 +0900

    front port 설정

--- src/main/java/com/soroksorokk/soroksorokk/config/CorsConfiguration.java
+++ src/main/java/com/soroksorokk/soroksorokk/config/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("*")
                         .allowCredentials(true);
             }
